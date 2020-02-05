96df1e6c5ebd572b2636e68253c61be116fd0241
Virjanand <virjanand@virjanand.com>
2020-02-05 05:25:57 +0100

    Refactor: remove duplication in unit tests

--- src/test/java/YahtzeeTest.java
+++ src/test/java/YahtzeeTest.java
@@ -1,19 +1,32 @@
+import org.assertj.core.api.AbstractIntegerAssert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class YahtzeeTest {
 
+    private Yahtzee yahtzee;
+
+    @BeforeEach
+    void createYahtzee() {
+        yahtzee = new Yahtzee();
+    }
+
     @Test
     void oneOneInCategoryOnes_Score1() {
-        Yahtzee yahtzee = new Yahtzee();
-        assertThat(yahtzee.score(asList(1, 2, 3, 4, 5))).isEqualTo(1);
+        assertRollsInCategoryGiveScore(asList(1, 2, 3, 4, 5), 1);
     }
 
     @Test
     void twoOnesInCategoryOnes_Score2() {
-        Yahtzee yahtzee = new Yahtzee();
-        assertThat(yahtzee.score(asList(1, 1, 3, 4, 5))).isEqualTo(2);
+        assertRollsInCategoryGiveScore(asList(1, 1, 3, 4, 5), 2);
+    }
+
+    private AbstractIntegerAssert<?> assertRollsInCategoryGiveScore(List<Integer> rolls, int score) {
+        return assertThat(yahtzee.score(rolls)).isEqualTo(score);
     }
 }
