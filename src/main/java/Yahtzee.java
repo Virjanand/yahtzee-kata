21b7d91b6c9ae290a0042a134972fdbbfcf69800
Virjanand <virjanand@virjanand.com>
2020-02-05 05:17:53 +0100

    Refactor: remove duplication 1 from test and 1 in code

--- src/main/java/Yahtzee.java
+++ src/main/java/Yahtzee.java
@@ -1,7 +1,10 @@
 import java.util.List;
 
 public class Yahtzee {
-    public int score(List<Integer> asList) {
-        return 1;
+    public int score(List<Integer> rolls) {
+        return rolls.stream()
+                .filter(number -> number == 1)
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 }
