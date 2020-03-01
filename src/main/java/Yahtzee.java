0fccd26059a642c7d6fdbbd90401c3caa105458c
Virjanand <virjanand@virjanand.com>
2020-03-01 16:10:55 +0100

    Refactor: make methods for filtering same

--- src/main/java/Yahtzee.java
+++ src/main/java/Yahtzee.java
@@ -1,26 +1,23 @@
 import java.util.List;
 
 public class Yahtzee {
-    private static boolean ones(Integer number) {
-        return number == 1;
-    }
-
     public int score(List<Integer> rolls, String category) {
         if (category.equals("Twos")) {
             return rolls.stream()
-                    .filter(Yahtzee::twos)
+                    .filter(number -> numberMatches(number, 2))
                     .mapToInt(Integer::intValue)
                     .sum();
 
         }
 
         return rolls.stream()
-                .filter(Yahtzee::ones)
+                .filter(number -> numberMatches(number, 1))
                 .mapToInt(Integer::intValue)
                 .sum();
     }
 
-    private static boolean twos(Integer number) {
-        return number == 2;
+    private static boolean numberMatches(Integer number, int matchingNumber) {
+        return number == matchingNumber;
     }
+
 }
