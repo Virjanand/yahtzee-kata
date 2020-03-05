19504c8ec904f277e83ba1a29acd643c65cdafa6
Virjanand <virjanand@virjanand.com>
2020-03-05 14:43:11 +0100

    Green test: refactor to remove duplication with test

--- src/main/java/categories/PairCategory.java
+++ src/main/java/categories/PairCategory.java
@@ -1,10 +1,19 @@
 package categories;
 
 import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public class PairCategory implements Category {
     @Override
     public int calculateScoreForNumber(List<Integer> rolls) {
-        return 4;
+        Map<Integer, Long> occurences = rolls.stream()
+                .collect(groupingBy(Integer::intValue, counting()));
+        return 2 * occurences.entrySet().stream()
+                .filter(entry -> entry.getValue() == 2)
+                .mapToInt(Map.Entry::getKey)
+                .max().orElse(0);
     }
 }
