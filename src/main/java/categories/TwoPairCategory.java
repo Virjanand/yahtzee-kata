17df849250198cc50961e2afab086d4a38409c21
Virjanand <virjanand@virjanand.com>
2020-03-07 13:48:25 +0100

    Red test: 2 pairs give sum of Dice as score

---
+++ src/main/java/categories/TwoPairCategory.java
@@ -0,0 +1,10 @@
+package categories;
+
+import java.util.List;
+
+public class TwoPairCategory implements Category {
+    @Override
+    public int calculateScoreForNumber(List<Integer> rolls) {
+        return 0;
+    }
+}
--- src/test/java/YahtzeeTest.java
+++ src/test/java/YahtzeeTest.java
@@ -1,6 +1,7 @@
 import categories.Category;
 import categories.NumberCategory;
 import categories.PairCategory;
+import categories.TwoPairCategory;
 import org.assertj.core.api.AbstractIntegerAssert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -36,7 +37,17 @@ public class YahtzeeTest {
 
     @Test
     void twoTwosInCategoryPair_Score4() {
-        assertRollsInCategoryGiveScore(asList(1, 2, 3, 2, 5), 4, new PairCategory());
+        assertRollsInCategoryGiveScore(asList(1, 2, 3, 2, 5), 2+2, new PairCategory());
+    }
+
+    @Test
+    void noPairInCategoryPair_Score0() {
+        assertRollsInCategoryGiveScore(asList(1, 2, 3, 4, 5), 0, new PairCategory());
+    }
+
+    @Test
+    void twoPairInCategoryTwoPair_ScoreSumOfDice() {
+        assertRollsInCategoryGiveScore(asList(1, 1, 3, 3, 5), 1+1+3+3, new TwoPairCategory());
     }
 
     private AbstractIntegerAssert<?> assertRollsInCategoryGiveScore(List<Integer> rolls, int score, Category category) {
