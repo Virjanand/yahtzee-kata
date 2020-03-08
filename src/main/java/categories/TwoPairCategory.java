439f46ac381662aa1fbd3c4952bdaecfc6ffb646
Virjanand <virjanand@virjanand.com>
2020-03-08 12:32:39 +0100

    Refactor: replace with real implementation copied from PairCategory

--- src/main/java/categories/TwoPairCategory.java
+++ src/main/java/categories/TwoPairCategory.java
@@ -1,10 +1,19 @@
 package categories;
 
 import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public class TwoPairCategory implements Category {
     @Override
     public int calculateScoreForNumber(List<Integer> rolls) {
-        return 8;
+        Map<Integer, Long> occurences = rolls.stream()
+                .collect(groupingBy(Integer::intValue, counting()));
+        return 2 * occurences.entrySet().stream()
+                .filter(entry -> entry.getValue() == 2)
+                .mapToInt(Map.Entry::getKey)
+                .sum();
     }
 }
