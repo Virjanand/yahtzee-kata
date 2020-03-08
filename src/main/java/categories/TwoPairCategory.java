c9483e34aaaa5ae42644199309b24e378c11a51c
Virjanand <virjanand@virjanand.com>
2020-03-08 12:39:58 +0100

    Refactor: extract part that is duplicate in TwoPair and Pair

--- src/main/java/categories/TwoPairCategory.java
+++ src/main/java/categories/TwoPairCategory.java
@@ -2,6 +2,7 @@ package categories;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
@@ -9,11 +10,14 @@ import static java.util.stream.Collectors.groupingBy;
 public class TwoPairCategory implements Category {
     @Override
     public int calculateScoreForNumber(List<Integer> rolls) {
-        Map<Integer, Long> occurences = rolls.stream()
-                .collect(groupingBy(Integer::intValue, counting()));
-        return 2 * occurences.entrySet().stream()
-                .filter(entry -> entry.getValue() == 2)
-                .mapToInt(Map.Entry::getKey)
+        return 2 * occurencesOfPairs(rolls)
                 .sum();
+    }
+
+    private IntStream occurencesOfPairs(List<Integer> rolls) {
+        return rolls.stream()
+                .collect(groupingBy(Integer::intValue, counting())).entrySet().stream()
+                .filter(entry -> entry.getValue() == 2)
+                .mapToInt(Map.Entry::getKey);
     }
 }
