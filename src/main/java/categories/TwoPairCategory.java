0d40764b1a70da69840610e7399a6316bba03f3a
Virjanand <virjanand@virjanand.com>
2020-03-14 12:13:21 +0100

    Green test: full house in twoPairCategory score 0

--- src/main/java/categories/TwoPairCategory.java
+++ src/main/java/categories/TwoPairCategory.java
@@ -1,12 +1,18 @@
 package categories;
 
 import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class TwoPairCategory implements Category {
     @Override
     public int calculateScoreForNumber(List<Integer> rolls) {
-        return 2 * RollCombinationUtil.occurencesOfPairs(rolls)
+        List<Integer> occurences = RollCombinationUtil.occurencesOfPairs(rolls).boxed().collect(toList());
+        if (occurences.size() == 2)
+        return 2 * occurences.stream().mapToInt(Integer::intValue)
                 .sum();
+        return 0;
     }
 
     @Override
