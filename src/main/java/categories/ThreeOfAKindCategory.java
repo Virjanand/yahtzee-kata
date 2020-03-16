0182fd3de5a4d8ab05e18c4100e0b2f3e2948afe
Virjanand <virjanand@virjanand.com>
2020-03-16 05:01:58 +0100

    Green Test: Three of a Kind with three 1's gives score 3

--- src/main/java/categories/ThreeOfAKindCategory.java
+++ src/main/java/categories/ThreeOfAKindCategory.java
@@ -1,10 +1,17 @@
 package categories;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ThreeOfAKindCategory implements Category{
     @Override
     public int calculateScoreForNumber(List<Integer> rolls) {
-        return 0;
+        return 3 * rolls.stream()
+                .collect(Collectors.groupingBy(Integer::intValue, Collectors.counting())).entrySet().stream()
+                .filter(entry -> entry.getValue() == 3)
+                .map(Map.Entry::getKey)
+                .mapToInt(Integer::intValue)
+                .max().orElse(0);
     }
 }
