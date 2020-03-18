d1910ed20c2066bb02e4f7ef99c11ce6f1d3c517
Virjanand <virjanand@virjanand.com>
2020-03-18 05:05:58 +0100

    Red Test: Four of a kind category with 4 1's gives score 4

---
+++ src/main/java/categories/FourOfAKindCategory.java
@@ -0,0 +1,10 @@
+package categories;
+
+import java.util.List;
+
+public class FourOfAKindCategory implements Category{
+    @Override
+    public int scoreForRollOf5Dice(List<Integer> rolls) {
+        return 0;
+    }
+}
