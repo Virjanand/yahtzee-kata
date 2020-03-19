7b78dce2fa39a61f9b641f1cb51d4d9fa80c8d64
Virjanand <virjanand@virjanand.com>
2020-03-19 06:37:46 +0100

    Green Test: Category SmallStraight with 1,2,3,4,5 gives sum as Score

--- src/main/java/categories/SmallStraightCategory.java
+++ src/main/java/categories/SmallStraightCategory.java
@@ -2,9 +2,12 @@ package categories;
 
 import java.util.List;
 
+import static java.util.Arrays.asList;
+
 public class SmallStraightCategory implements Category{
     @Override
     public int scoreForRollOf5Dice(List<Integer> rolls) {
-        return 0;
+        List<Integer> smallStraight = asList(1, 2, 3, 4, 5);
+        return smallStraight.equals(rolls) ? 15 : 0;
     }
 }
