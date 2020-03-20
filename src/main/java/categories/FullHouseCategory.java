2a8e38642a49ab1251a0417373bcf6211ad1f479
Virjanand <virjanand@virjanand.com>
2020-03-20 05:35:32 +0100

    Refactor use pair and three of a kind to calculate score full house

--- src/main/java/categories/FullHouseCategory.java
+++ src/main/java/categories/FullHouseCategory.java
@@ -5,6 +5,11 @@ import java.util.List;
 public class FullHouseCategory implements Category{
     @Override
     public int scoreForRollOf5Dice(List<Integer> rolls) {
-        return 8;
+        int pair = RollCombinationUtil.scoreForNumberOfOccurrences(rolls, 2);
+        int threeOfAKind = RollCombinationUtil.scoreForNumberOfOccurrences(rolls, 3);
+        if (pair == 0 && threeOfAKind == 0) {
+            return 0;
+        }
+        return pair + threeOfAKind;
     }
 }
