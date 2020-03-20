5ecaddc0d9a388e9fd16a31a6af629f13a905930
Virjanand <virjanand@virjanand.com>
2020-03-20 05:30:52 +0100

    Green Test: Category LargeStraight with 2,3,4,5,6 gives sum as Score

--- src/main/java/categories/LargeStraightCategory.java
+++ src/main/java/categories/LargeStraightCategory.java
@@ -2,9 +2,12 @@ package categories;
 
 import java.util.List;
 
+import static java.util.Arrays.asList;
+
 public class LargeStraightCategory implements Category {
     @Override
     public int scoreForRollOf5Dice(List<Integer> rolls) {
-        return 0;
+        List<Integer> smallStraight = asList(2, 3, 4, 5, 6);
+        return smallStraight.equals(rolls) ? 20 : 0;
     }
 }
